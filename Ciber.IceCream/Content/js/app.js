57b4e798fd684c51b8fbab036efe3b3a007e0168
Marius Gundersen <gundersen@gmail.com>
2013-06-17 22:53:44 +0200

    added view content

---
+++ Ciber.IceCream/Content/js/app.js
@@ -0,0 +1,32 @@
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  this.resource('buyIceCream', { path: '/' });
+  this.resource('fillFreezer');
+});
+
+
+App.BuyIceCreamRoute = Ember.Route.extend({
+  model: function() {
+    return [
+		{
+			id: 1,
+			title: 'Krone-is Jordbær',
+			image: "images/is_1.jpg",
+			price: 8
+		},
+		{
+			id: 2,
+			title: 'Lollipop',
+			image: "images/is_2.jpg",
+			price: 3
+		},
+		{
+			id: 3,
+			title: 'Snickers is',
+			image: "images/is_3.jpg",
+			price: 9
+		}
+    ];
+  }
+});
