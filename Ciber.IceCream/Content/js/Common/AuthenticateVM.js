1e806ce10ec799706850c7b19a21fbb59ead518a
Marius Gundersen <gundersen@gmail.com>
2013-06-19 16:12:53 +0200

    Mucking about with popups and stuff. This is getting terribly complex...

---
+++ Ciber.IceCream/Content/js/Common/AuthenticateVM.js
@@ -0,0 +1,17 @@
+﻿define(["knockout", "ordnung/ajax"], function (ko, ajax) {
+
+    function AuthenticateVM(_authenticate) {
+
+        var self = this;
+
+        this.buyerId = ko.observable();
+
+        this.authenticate = function() {
+            _authenticate();
+        };
+    }
+
+
+    return AuthenticateVM;
+
+});
---
+++ Ciber.IceCream/Content/js/Service/currentUser.js
@@ -0,0 +1,27 @@
+﻿define(["Service/popupService", "Common/AuthenticateVM"], function(popupService, AuthenticateVM) {
+
+    var loggedIn = false;
+    var userId = null;
+
+    function authenticate(whenDone) {
+        if (loggedIn) {
+            whenDone(true, userId);
+        } else {
+            var authenticateVM = new AuthenticateVM();
+            var popup = popupService.createPopup("AuthenticationPopup", authenticateVM, function (success) {
+                if (success) {
+                    userId = authenticateVM.buyerId();
+                    loggedIn = true;
+                    whenDone(true, userId);
+                } else {
+                    whenDone(false);
+                }
+            }).open();
+        }
+    }
+
+    return {
+        authenticate: authenticate
+    };
+
+});
