6d3ecebae7fbf756a2657642b275621bc815df22
Per-Kristian Helland <perkristianhelland@gmail.com>
2013-06-24 10:53:06 +0200

    Hide logout if not logged in.

--- Ciber.IceCream/Content/js/Common/NavBarVM.js
+++ Ciber.IceCream/Content/js/Common/NavBarVM.js
@@ -1,16 +1,21 @@
-﻿define(["Service/currentUser"], function (currentUser) {
+﻿define(["Service/currentUser", "knockout"], function (currentUser, ko) {
+
 
     function NavBarVM() {
 
         var self = this;
-
+        var setIsLoggedIn = function() {
+            self.isLoggedIn(!!localStorage.getItem("CiberIceUserId"));
+        };
+        this.isLoggedIn = ko.observable(false);
 
         this.logout = function () {
             currentUser.logout();
+            setIsLoggedIn();
         };
 
         init: {
-
+            setIsLoggedIn();
         }
 
     }
