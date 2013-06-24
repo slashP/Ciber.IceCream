399aba81cf9840d8edd680b49976cd448fb60067
Per-Kristian Helland <perkristianhelland@gmail.com>
2013-06-24 15:35:54 +0200

    Tests for buy ice

---
+++ Ciber.IceCream.Test/BuyControllerTest.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using CiberIs.Controllers;
+using CiberIs.Models;
+using FakeItEasy;
+using MongoDB.Driver;
+using NUnit.Framework;
+
+namespace Ciber.IceCream.Test
+{
+    [TestFixture]
+    public class BuyControllerTest
+    {
+        private BuyController _controller;
+        private IMongoDb _mongoDb;
+
+        [SetUp]
+        public void Init()
+        {
+            _mongoDb = A.Fake<IMongoDb>();
+            _controller = new BuyController(_mongoDb);
+        }
+
+        [Test]
+        public void BuyIceThrowsHttpExceptionIfThereIsNoSuchIce()
+        {
+            A.CallTo(() => _mongoDb.FindById<CiberIs.Models.IceCream>(A<string>.Ignored, A<string>.Ignored))
+             .Returns(null);
+            Assert.Throws<HttpResponseException>(
+                () => _controller.Post(new FormDataCollection(Enumerable.Empty<KeyValuePair<string, string>>())));
+        }
+
+        [Test]
+        public void BuyIceThrowsHttpExceptionIfThereAreZeroIcesLeft()
+        {
+            var ice = new CiberIs.Models.IceCream {Quantity = 0};
+            A.CallTo(() => _mongoDb.FindById<CiberIs.Models.IceCream>(A<string>.Ignored, A<string>.Ignored))
+             .Returns(ice);
+            var exception =
+                Assert.Throws<HttpResponseException>(
+                    () => _controller.Post(new FormDataCollection(Enumerable.Empty<KeyValuePair<string, string>>())));
+            Assert.AreEqual(exception.Response.StatusCode, HttpStatusCode.Conflict);
+        }
+
+        [Test]
+        public void BuyIceCallsSaveWithUpdatedQuantity()
+        {
+            var ice = new CiberIs.Models.IceCream {Quantity = 2};
+            A.CallTo(() => _mongoDb.FindById<CiberIs.Models.IceCream>(A<string>.Ignored, A<string>.Ignored))
+             .Returns(ice);
+            var keyValuePairs = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("buyer", "561")
+                };
+            _controller.Post(new FormDataCollection(keyValuePairs));
+            A.CallTo(() => _mongoDb.Save(A<CiberIs.Models.IceCream>.Ignored, A<string>.Ignored)).MustHaveHappened();
+        }
+
+        [Test]
+        public void BuyIceReducesQuantityWithOne()
+        {
+            var ice = new CiberIs.Models.IceCream {Quantity = 2};
+            A.CallTo(() => _mongoDb.FindById<CiberIs.Models.IceCream>(A<string>.Ignored, A<string>.Ignored))
+             .Returns(ice);
+            var keyValuePairs = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("buyer", "561")
+                };
+            _controller.Post(new FormDataCollection(keyValuePairs));
+            Assert.AreEqual(1, ice.Quantity);
+        }
+
+        [Test]
+        public void BuyIceInsertsPurchase()
+        {
+            var ice = new CiberIs.Models.IceCream {Quantity = 2, Price = 7};
+            A.CallTo(() => _mongoDb.FindById<CiberIs.Models.IceCream>(A<string>.Ignored, A<string>.Ignored))
+             .Returns(ice);
+            var keyValuePairs = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("buyer", "561")
+                };
+            _controller.Post(new FormDataCollection(keyValuePairs));
+            A.CallTo(
+                () =>
+                _mongoDb.Insert(A<Purchase>.That.Matches(x => x.Buyer == 561 && x.Price == ice.Price), "Purchases"))
+             .MustHaveHappened();
+        }
+
+        [Test]
+        public void BuyIceWhenExceptionReturnsSuccssFalse()
+        {
+            var ice = new CiberIs.Models.IceCream {Quantity = 2, Price = 7};
+            A.CallTo(() => _mongoDb.FindById<CiberIs.Models.IceCream>(A<string>.Ignored, A<string>.Ignored))
+             .Returns(ice);
+            A.CallTo(() => _mongoDb.Save(A<CiberIs.Models.IceCream>.Ignored, A<string>.Ignored))
+             .Throws(new MongoException("sorry"));
+            var keyValuePairs = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("buyer", "561")
+                };
+            var result = _controller.Post(new FormDataCollection(keyValuePairs));
+            Assert.IsFalse(result.success);
+        }
+
+        [Test]
+        public void BuyIceWhenNothingIsWrongReturnsSuccssTrue()
+        {
+            var ice = new CiberIs.Models.IceCream {Quantity = 2, Price = 7};
+            A.CallTo(() => _mongoDb.FindById<CiberIs.Models.IceCream>(A<string>.Ignored, A<string>.Ignored))
+             .Returns(ice);
+            var keyValuePairs = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("buyer", "561")
+                };
+            var result = _controller.Post(new FormDataCollection(keyValuePairs));
+            Assert.IsTrue(result.success);
+        }
+    }
+}
--- Ciber.IceCream/Controllers/BuyController.cs
+++ Ciber.IceCream/Controllers/BuyController.cs
@@ -3,25 +3,29 @@ using System.Net;
 using System.Net.Http.Formatting;
 using System.Web.Http;
 using CiberIs.Models;
-using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace CiberIs.Controllers
 {
     public class BuyController : ApiController
     {
-        private readonly MongoDatabase _mongoDb = MongoHqConfig.RetrieveMongoHqDb();
+        private readonly IMongoDb _mongoDb;
+
+        public BuyController(IMongoDb mongoDb)
+        {
+            _mongoDb = mongoDb;
+        }
 
         public dynamic Post(FormDataCollection data)
         {
-            var ice = _mongoDb.GetCollection<IceCream>("IceCreams").FindOneById(new ObjectId(data.Get("iceCreamId")));
+            var ice = _mongoDb.FindById<IceCream>(data.Get("iceCreamId"), "IceCreams");
             if (ice == null) throw new HttpResponseException(HttpStatusCode.ExpectationFailed);
             try
             {
                 ice.Quantity--;
                 if(ice.Quantity < 0) throw new HttpResponseException(HttpStatusCode.Conflict);
-                _mongoDb.GetCollection<IceCream>("IceCreams").Save(ice);
-                _mongoDb.GetCollection<Purchase>("Purchases").Insert(new Purchase() { Price = ice.Price, Buyer = int.Parse(data.Get("buyer")), Time = DateTime.UtcNow });
+                _mongoDb.Save(ice, "IceCreams");
+                _mongoDb.Insert(new Purchase() { Price = ice.Price, Buyer = int.Parse(data.Get("buyer")), Time = DateTime.UtcNow }, "Purchases");
             }
             catch (MongoException e)
             {
