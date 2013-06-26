b5606cbdac7427959aff36a1c30c549c1ad20edd
Per-Kristian Helland <perkristianhelland@gmail.com>
2013-06-26 17:07:24 +0200

    Cannot start badge calcs simultaneously. Hacked with tasks.

--- Ciber.IceCream/Global.asax.cs
+++ Ciber.IceCream/Global.asax.cs
@@ -1,4 +1,8 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -6,6 +10,7 @@ using System.Web.Routing;
 using CiberIs.Badges;
 using CiberIs.Models;
 using EmptyMvc4.Models;
+using CiberIs.Extensions;
 
 namespace CiberIs
 {
@@ -23,11 +28,20 @@ namespace CiberIs
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             Database.SetInitializer(new CreateDatabaseIfNotExists<UsersContext>());
-            new MorningGloryBadge((IMongoDb)GlobalConfiguration.Configuration.DependencyResolver.GetService(typeof(IMongoDb)));
-            new MedalsBadge((IMongoDb)GlobalConfiguration.Configuration.DependencyResolver.GetService(typeof(IMongoDb)));
-            new ConsistentBadge((IMongoDb)GlobalConfiguration.Configuration.DependencyResolver.GetService(typeof(IMongoDb)));
-            new OneFavouriteBadge((IMongoDb)GlobalConfiguration.Configuration.DependencyResolver.GetService(typeof(IMongoDb)));
-            new MixItUpBadge((IMongoDb)GlobalConfiguration.Configuration.DependencyResolver.GetService(typeof(IMongoDb)));
+            var badges = new List<Type>
+                {
+                    typeof (MorningGloryBadge),
+                    typeof (MedalsBadge),
+                    typeof (ConsistentBadge),
+                    typeof (OneFavouriteBadge),
+                    typeof (MixItUpBadge)
+                };
+            badges.Each((x, i) => Task.Factory.StartNew(() =>
+                {
+                    var mongoDb = (IMongoDb) GlobalConfiguration.Configuration.DependencyResolver.GetService(typeof (IMongoDb));
+                    Thread.Sleep(30000 * i);
+                    Activator.CreateInstance(x, mongoDb);
+                }));
         }
     }
 }
