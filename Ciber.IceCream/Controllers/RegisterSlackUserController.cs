a319caa51ff2e64caaab5d227b2fcfaa9106831f
Per-Kristian Helland <perkristianhelland@gmail.com>
2015-04-29 19:32:17 +0200

    Made it possible to register slack user with employee id.

---
+++ Ciber.IceCream/Controllers/RegisterSlackUserController.cs
@@ -0,0 +1,31 @@
+﻿namespace CiberIs.Controllers
+{
+    using System.Linq;
+    using System.Web.Http;
+
+    using CiberIs.Models;
+
+    using MongoDB.Driver.Builders;
+
+    public class RegisterSlackUserController : ApiController
+    {
+        private IMongoDb _db;
+
+        public RegisterSlackUserController(IMongoDb mongoDb)
+        {
+            _db = mongoDb;
+        }
+
+        public dynamic Post(string slackId, int employeeId)
+        {
+            var existingUser = _db.GetCollection<SlackUser>("SlackUsers").FirstOrDefault(x => x.SlackUserId == slackId);
+            if (existingUser != null)
+            {
+                MongoHqConfig.RetrieveMongoHqDb().GetCollection<SlackUser>("SlackUsers").Remove(Query.EQ("_id", existingUser.Id));
+            }
+
+            _db.Insert(new SlackUser { SlackUserId = slackId, EmployeeId = employeeId }, "SlackUsers");
+            return new{success = true};
+        }
+    }
+}
---
+++ Ciber.IceCream/Models/SlackUser.cs
@@ -0,0 +1,16 @@
+﻿namespace CiberIs.Models
+{
+    using MongoDB.Bson;
+
+    using Newtonsoft.Json;
+
+    public class SlackUser
+    {
+        [JsonIgnore]
+        public BsonObjectId Id { get; set; }
+
+        public string SlackUserId { get; set; }
+
+        public int EmployeeId { get; set; }
+    }
+}
