919fd7f079bbe7ca34343ed09eebad9b93124888
Per-Kristian Helland <perkristianhelland@gmail.com>
2015-06-12 12:22:05 +0200

    Removing all loss functionality as it only creates chaos.

--- Ciber.IceCream/Models/IceCream.cs
+++ Ciber.IceCream/Models/IceCream.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 using MongoDB.Bson;
 
 namespace CiberIs.Models
@@ -11,6 +12,7 @@ namespace CiberIs.Models
         public string Title { get; set; }
         public int Quantity { get; set; }
         public string Image { get; set; }
+        [Obsolete("Removed loss functionality. Here for backwards compatibility")]
         public int? Loss { get; set; }
     }
 }
